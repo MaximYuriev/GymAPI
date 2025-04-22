8b096f3bacbc00587b33744adfb3cb05ddfacd64
Maxim <yrev2011@gmail.com>
2025-04-22 19:27:16 +0700

    add test for create customer command

--- test/application/conftest.py
+++ test/application/conftest.py
@@ -1,7 +1,11 @@
+import random
+
 import pytest
 from dishka import AsyncContainer
 
+from src.application.commands.customer import CreateCustomerCommand
 from src.application.commands.ticket import CreateTicketTypeCommand
+from src.application.handlers.commands.customer import CreateCustomerCommandHandler
 from src.application.handlers.commands.ticket import CreateTicketTypeCommandHandler
 from src.application.handlers.queries.ticket import GetAllTicketTypesQueryHandler
 from src.application.mediator import Mediator
@@ -17,10 +21,23 @@ def mediator() -> Mediator:
         command=CreateTicketTypeCommand,
         command_handler=CreateTicketTypeCommandHandler,
     )
+    mediator.register_command(
+        command=CreateCustomerCommand,
+        command_handler=CreateCustomerCommandHandler,
+    )
 
     mediator.register_query(
         query=GetAllTicketTypesQuery,
         query_handler=GetAllTicketTypesQueryHandler,
     )
 
+
     return mediator
+
+
+@pytest.fixture
+def fake_phone_number() -> str:
+    value = "89"
+    for _ in range(9):
+        value += str(random.randint(0, 9))
+    return value
---
+++ test/application/test_customer.py
@@ -0,0 +1,66 @@
+import random
+
+import pytest
+from faker.proxy import Faker
+
+from src.application.commands.customer import CreateCustomerCommand
+from src.application.exceptions.customer import PhoneNumberNotUniqueException
+from src.application.mediator import Mediator
+from src.domain.entities.customer import Customer
+
+
+@pytest.mark.usefixtures("prepare_database")
+async def test_create_customer_success(
+        mediator: Mediator,
+        fake_phone_number: str,
+        faker: Faker,
+):
+    command = CreateCustomerCommand(
+        name=faker.name(),
+        surname=faker.name(),
+        patronymic=faker.name(),
+        phone=fake_phone_number,
+    )
+
+    customer: Customer = await mediator.handle_command(command)
+
+    assert customer.name.value == command.name
+    assert customer.phone.value == command.phone
+
+
+@pytest.mark.usefixtures("prepare_database")
+async def test_create_customer_w_null_patronymic(
+        mediator: Mediator,
+        fake_phone_number: str,
+        faker: Faker,
+):
+    command = CreateCustomerCommand(
+        name=faker.name(),
+        surname=faker.name(),
+        patronymic=None,
+        phone=fake_phone_number,
+    )
+
+    customer: Customer = await mediator.handle_command(command)
+
+    assert customer.patronymic.value is None
+    assert customer.name.value == command.name
+    assert customer.phone.value == command.phone
+
+
+@pytest.mark.usefixtures("prepare_database")
+async def test_create_customer_w_not_unique_phone_number(
+        mediator: Mediator,
+        fake_phone_number: str,
+        faker: Faker,
+):
+    command = CreateCustomerCommand(
+        name=faker.name(),
+        surname=faker.name(),
+        patronymic=random.choice([None, faker.name()]),
+        phone=fake_phone_number,
+    )
+    await mediator.handle_command(command)
+
+    with pytest.raises(PhoneNumberNotUniqueException):
+        await mediator.handle_command(command)
